062194a2409eea8cb67d68b629125abef0a2af2f
Yasi-Philippe <shyasi95@gmail.com>
2026-01-13 15:23:01 +0100

    fix: Fixed negative values

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print(f"Last digit of {number} is {abs(number % 10)} and is ", end = "")
-if number == 0:
+if number < 0:
+    last_digit = number % -10
+else:
+    last_digit = number % 10
+print(f"Last digit of {number} is {last_digit} and is ", end="")
+if last_digit == 0:
     print(0)
-elif number % 10 > 5:
+elif last_digit > 5:
     print("greater than 5")
 else:
     print("less than 6 and not 0")
