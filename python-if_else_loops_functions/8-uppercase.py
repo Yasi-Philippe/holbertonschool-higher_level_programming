0a0da69f2dedeb146be747ea0d3f5d51a6823d30
Yasi-Philippe <shyasi95@gmail.com>
2026-01-14 10:35:07 +0100

    feat: Python script that turns a string into uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    str += "\n"
+    for i in str:
+        if 96 < ord(i) < 123:
+            print(chr(ord(i) - 32), end="")
+        else:
+            print(i, end="")
