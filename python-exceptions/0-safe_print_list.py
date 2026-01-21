6867543a866420f3f82ab2a76c7f1455a6b500bf
Yasi-Philippe <shyasi95@gmail.com>
2026-01-21 11:35:37 +0100

    fix: Added a new line after printing the list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end ="")
             lenght += 1
         except:
-            print()
             break
+    print()
     return lenght
