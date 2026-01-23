353f856143e6775adc9d3a3d177cceed05fa9250
Yasi-Philippe <shyasi95@gmail.com>
2026-01-23 15:30:07 +0100

    feat: Python function that prints the name and last name

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Docstring for 3-say_my_name
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Docstring for say_my_name
+    :param first_name: First Name
+    :param last_name: Last Name
+    """
+
+    if not first_name:
+        raise TypeError("first_name must be a string")
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
