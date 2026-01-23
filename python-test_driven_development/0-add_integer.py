ac78bee8f4b2bbd06949f6e900153ecf622c6285
Yasi-Philippe <shyasi95@gmail.com>
2026-01-23 14:13:16 +0100

    fix: Added exception error for FLoat Overflow

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,12 +10,15 @@ def add_integer(a, b=98):
     :param a: First integer to sum
     :param b: Second integer to sum
     """
-    if a is None:
-        raise TypeError("a must be an integer")
-    if type(a) is not int and type(a) is not float:
-        raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
-        raise TypeError("b must be an integer")
-    a = int(a)
-    b = int(b)
-    return a + b
+    try:
+        if a is None:
+            raise TypeError("a must be an integer")
+        if type(a) is not int and type(a) is not float:
+            raise TypeError("a must be an integer")
+        if type(b) is not int and type(b) is not float:
+            raise TypeError("b must be an integer")
+        a = int(a)
+        b = int(b)
+        return a + b
+    except SyntaxError:
+        raise SyntaxError
