4a41d80017c1e0a1fa6afff32a47f647388870db
Yasi-Philippe <shyasi95@gmail.com>
2026-01-23 18:06:41 +0100

    feat: Python function that handles exceptions parsing a text

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Docstring for 3-text_indentation
+"""
+
+
+def text_indentation(text):
+    """
+    Docstring for text_indentation
+    :param text: Input text to print
+    """
+
+    if not text:
+        raise TypeError("text must be a string")
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    for character in text:
+        if character not in (".", "?", ":"):
+            print(character, end="")
+        else:
+            print("{}\n\n".format(character))
