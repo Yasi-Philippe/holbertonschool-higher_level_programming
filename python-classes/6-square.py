2010a7ed9aa518270d7bff95477b615461e74b88
Yasi-Philippe <shyasi95@gmail.com>
2026-01-28 06:44:09 +0100

    feat: Python class that defines a square. Can retrieve the are and print the square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Square"""
+
+
 class Square:
     """Class that defines a square, notably it's size"""
     def __init__(self, size=0, position=(0, 0)):
@@ -7,22 +9,56 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        if not isinstance(position, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(position[0], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(position[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__size = size
+        self.__position = position
+
     @property
     def size(self):
+        """Function getter for size"""
         return self.__size
+
+    @property
+    def position(self):
+        """Function getter for position"""
+        return self.__position
+
     @size.setter
     def size(self, value):
+        """Function setter for size"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    @position.setter
+    def position(self, value):
+        """Function setter that sets the position of the square"""
+        if not isinstance(value, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(value[0], int) or not isinstance(value[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
     def area(self):
+        """Function that retrieves the area of the square"""
         return self.__size ** 2
+
     def my_print(self):
+        """Function that prints the square of the size saved.
+        Position of the square will be aletered by
+        the saved position attribute"""
         if self.__size == 0:
             print()
         else:
+            for _ in range(self.__position[1]):
+                print()
             for _ in range(self.__size):
+                print(" " * self.__position[0], end="")
                 print(self.__size * "#")
