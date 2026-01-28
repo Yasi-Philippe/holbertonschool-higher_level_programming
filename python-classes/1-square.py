70579ecfe243131d11cf6723c2265437d0d6eef2
Yasi-Philippe <shyasi95@gmail.com>
2026-01-28 06:15:16 +0100

    fix: Added comments

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Square"""
+
+
 class Square:
     """Class that defines a square, notably it's size"""
     def __init__(self, size):
+        """Function that initializes the size"""
         self.__size = size
