ccbb3849e8b33d4fbaaeccbae8dc32e7a08bd1fc
Yasi-Philippe <shyasi95@gmail.com>
2026-02-02 17:33:04 +0100

    feat: Python script that returns a list with all the attributes and methods of a class

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Script that lists all the attributes and mehtods of a class"""
+
+
+def lookup(obj):
+    """Function that lists all the attributes and methods of a class"""
+    return dir(obj)
