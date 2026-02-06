11b7154484db175fd58ae9571a3c7d10e2fa5e3f
Yasi-Philippe <shyasi95@gmail.com>
2026-02-06 14:44:46 +0100

    fix: Added a class comment

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,6 +3,8 @@
 
 
 class MyList(list):
+    """Class that sorts a list"""
+
     def print_sorted(self):
         """Function that returns a list of all the methods of an object sorted"""
         print(sorted(self))
