aa04b65e646be8db9ba55defea5cdab01a0f2060
Yasi-Philippe <shyasi95@gmail.com>
2026-02-13 10:22:02 +0100

    fix: Now appends the data in a loop

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -8,10 +8,13 @@ import json
 
 def convert_csv_to_json(filename):
     """Function that takes a csv file and turns it into json"""
+    data = []
     try:
         with open(filename, "r", encoding="UTF-8") as my_file:
-            data = list(csv.DictReader(my_file))
+            data_row = csv.DictReader(my_file)
+            for row in data_row:
+                data.append(row)
         with open("data.json", "w", enconding="UTF-8") as json_file:
             json.dump(data, json_file)
-    except Exception:
+    except FileNotFoundError:
         pass
