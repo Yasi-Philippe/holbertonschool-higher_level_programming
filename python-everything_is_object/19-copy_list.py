4373d9e1c42dd2c9fea676bd3a8eaff96311e59a
Yasi Philippe Hübner <shyasi95@gmail.com>
2026-03-31 15:32:22 +0200

    feat: Doc files

---
+++ python-everything_is_object/19-copy_list.py
@@ -0,0 +1,14 @@
+ def copy_list(a_list):
+    """Returns a copy of the given list.
+
+    Parameters
+    ----------
+    a_list : list
+        The list to copy.
+
+    Returns
+    -------
+    list
+        A copy of the given list.
+    """
+    return a_list[:]
